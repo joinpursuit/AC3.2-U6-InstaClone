79d6d0f6ed5b61365d8fe2eb1af491e2a6982031
Tom Seymour <tomeseymour44@gmail.com>
2017-02-07 11:26:30 -0500

    created vc for profileView

---
+++ InstaClone/ProfileViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ProfileViewController.swift
+//  InstaClone
+//
+//  Created by Tom Seymour on 2/7/17.
+//  Copyright © 2017 C4Q-3.2. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+}

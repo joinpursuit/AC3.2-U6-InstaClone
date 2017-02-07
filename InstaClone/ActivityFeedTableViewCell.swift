18aba82d3fd6a6197c6e387b59a9bf77dcf280fa
Tom Seymour <tomeseymour44@gmail.com>
2017-02-07 15:26:45 -0500

    login screen and profile screen, ready for real data

---
+++ InstaClone/ActivityFeedTableViewCell.swift
@@ -0,0 +1,78 @@
+//
+//  ActivityFeedTableViewCell.swift
+//  InstaClone
+//
+//  Created by Tom Seymour on 2/7/17.
+//  Copyright © 2017 C4Q-3.2. All rights reserved.
+//
+
+import UIKit
+import SnapKit
+
+class ActivityFeedTableViewCell: UITableViewCell {
+    
+    
+    
+    override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        setUpView()
+        setUpConstraints()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    
+    func setUpView() {
+        self.contentView.addSubview(profileImageView)
+        self.contentView.addSubview(activityTextLabel)
+        self.contentView.addSubview(activityDateLabel)
+    }
+    
+    func setUpConstraints() {
+        profileImageView.snp.makeConstraints { (view) in
+            view.leading.top.equalToSuperview().offset(4)
+            view.bottom.equalToSuperview().offset(-4)
+            view.width.equalTo(profileImageView.snp.height)
+        }
+        
+        activityTextLabel.snp.makeConstraints { (view) in
+            view.leading.equalTo(profileImageView.snp.trailing).offset(8)
+            view.centerY.equalToSuperview()
+        }
+        
+        activityDateLabel.snp.makeConstraints { (view) in
+            view.trailing.equalToSuperview().offset(-4)
+            view.centerY.equalToSuperview()
+        }
+    }
+
+    
+    lazy var profileImageView: UIImageView = {
+        let view = UIImageView()
+        view.image = UIImage(named: "user_icon")
+        view.backgroundColor = UIColor.instaAccent()
+        
+        view.layer.borderWidth = 1
+        view.layer.masksToBounds = false
+        view.layer.borderColor = UIColor.instaPrimaryDark().cgColor
+        view.layer.cornerRadius = 17
+        view.clipsToBounds = true
+        return view
+    }()
+    
+    lazy var activityTextLabel: UILabel = {
+        let view = UILabel()
+       return view
+    }()
+    
+    lazy var activityDateLabel: UILabel = {
+        let view = UILabel()
+        
+        return view
+    }()
+    
+    
+
+}
